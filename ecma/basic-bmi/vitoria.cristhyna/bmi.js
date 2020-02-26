b0f5fd1d5aaf967e98e4f1d212ddb7fd05d223c4
VitoriaSantos29 <61083461+VitoriaSantos29@users.noreply.github.com>
2020-02-26 14:57:57 -0300

    alteração na pasta (#27)
    * primeira alteracao
    
    * ajeitando arquivos
    
    * adicionando pastas
    
    * exercicio hello e começo do bmi
    
    * exercicios 1 e 2

---
+++ ecma/basic-bmi/vitoria.cristhyna/bmi.js
@@ -0,0 +1,17 @@
+const weight = 200;
+const height = 1.5;
+const bmi = weight/height**2;
+let result;
+if(bmi <=18.5){
+    result = "Underweight";
+}
+else if(bmi <=24.9){
+    result = "Normal weight"
+}
+else if(bmi <=29.9){
+    result = "Overweight";
+}
+else{
+    result = "Obesity";
+}	
+console.log(`BMI = ${result}`);
---
+++ ecma/basic-hello/vitoria.cristhyna/hello.js
@@ -0,0 +1,3 @@
+const name = "Vi";
+console.log(`Oi, ${name}!`);
+//const, var, let
